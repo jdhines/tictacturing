bb31df92c5df404582d65e9e521ac849a933b641
Jess Hines <jessdhines@gmail.com>
2017-04-12 09:46:23 -0400

    Add Material-UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,17 +3,30 @@ import { Route, IndexRoute } from 'react-router';
 import Home from '../containers/Home';
 import Profile from '../containers/Profile';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+injectTapEventPlugin();
+
 const createRoutes = () => {
   return (
-    <div>
-      <header>
-        <h1>TicTacTuring</h1>
-      </header>
-      <main>
-        <Route exact path="/" component={Home} />
-        <Route path="/profile" component={Profile} />
-      </main>
-    </div>
+    <MuiThemeProvider>
+      <div>
+        <header>
+          <h1>TicTacTuring</h1>
+          <RaisedButton
+            label="test button"
+            primary={true}
+            onTouchTap={()=>{console.log('hello, I work')}}
+          />
+        </header>
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/profile" component={Profile} />
+        </main>
+      </div>
+    </MuiThemeProvider>
   )
 }
 
